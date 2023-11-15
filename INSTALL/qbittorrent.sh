8f569378b7ccee94be13209604d0b9efdb6f1765
operador <brito.n@outlook.pt>
2023-11-15 18:16:04 +0000

    Updated INSTALL/qbittorrent.sh.

--- INSTALL/qbittorrent.sh
+++ INSTALL/qbittorrent.sh
@@ -1,4 +1,12 @@
+#!/bin/sh
+
 # Prerequisites
+## Init functions
+. /etc/os-release
+. initfunctions.sh
+func_def_distro
+func_def_host
+
 ## Global variables
 qbittorrent_ug = "qbtnoxuser"
 qbittorrent_service = "/etc/systemd/system/qbittorrent-nox.service"
